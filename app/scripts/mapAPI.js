51a7f8942de114c87614c69abbd1282693c77d5e
Taylor Smith <tsmit182@gmail.com>
2014-06-25 11:52:32 -0400

    add map parameters

--- app/scripts/mapAPI.js
+++ app/scripts/mapAPI.js
@@ -1,4 +1,9 @@
 function initialize () {
 	var map_canvas = document.getElementById('map_canvas');
+	var mapOptions = {
+		center: new google.maps.LatLng(40.751132, -73.855449),
+		zoom: 5,
+		mapTypeId: google.maps.MapTypeId.HYBRID
+	}
 	var map = new google.maps.Map(map_canvas);
 }
