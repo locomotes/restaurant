8b577cf9894708faa43be550ab2ca180e240516e
Taylor Smith <tsmit182@gmail.com>
2014-06-25 12:07:45 -0400

    added initialize; map is displaying properly! :-)

--- app/scripts/mapAPI.js
+++ app/scripts/mapAPI.js
@@ -1,9 +1,21 @@
 function initialize () {
 	var map_canvas = document.getElementById('map_canvas');
-	var mapOptions = {
+	var map_options = {
 		center: new google.maps.LatLng(40.751132, -73.855449),
-		zoom: 5,
+		zoom: 15,
 		mapTypeId: google.maps.MapTypeId.HYBRID
 	}
-	var map = new google.maps.Map(map_canvas);
+	var map = new google.maps.Map(map_canvas, map_options)
 }
+google.maps.event.addDomListener(window, 'load', initialize);
+
+// function initialize() {
+//   var map_canvas = document.getElementById('map_canvas');
+//   var map_options = {
+//     center: new google.maps.LatLng(44.5403, -78.5463),
+//     zoom: 8,
+//     mapTypeId: google.maps.MapTypeId.ROADMAP
+//   }
+//   var map = new google.maps.Map(map_canvas, map_options)
+// }
+// google.maps.event.addDomListener(window, 'load', initialize);
