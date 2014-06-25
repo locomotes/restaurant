994b6d22aecc87bff64b7d3ed99ef6aa6a54e9b3
Taylor Smith <tsmit182@gmail.com>
2014-06-25 13:59:40 -0400

    remove map template; remove googleAPI's example text in mapAPI

--- app/scripts/map.js
+++
@@ -1,5 +0,0 @@
-// var mapTemplate=_.template($('#mapTemplate').html() );
-
-// var map= $.getJSON('http://docs.restaurantapi.apiary.io/').done(function (data){ 
-// 	$('#googleMap').html(mapTemplate(data));
-// });
