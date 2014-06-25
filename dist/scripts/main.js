2623a4353df577bc458bc034a48c14816ffed423
Taylor Smith <tsmit182@gmail.com>
2014-06-25 17:37:35 -0400

    added commented-out workings towards resizing map on hover

--- dist/scripts/main.js
+++ dist/scripts/main.js
@@ -18,3 +18,11 @@ function initialize () {
 	var map = new google.maps.Map(map_canvas, map_options)
 }
 google.maps.event.addDomListener(window, 'load', initialize);
+
+// $('#map_canvas').hover()function({
+
+// 	$(this).addClass('hover');
+
+// })
+
+// ^^Check syntax, but this should work
