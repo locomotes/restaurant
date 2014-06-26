8cabde4ea750c67942c72349e929fed44a95d94b
Katlyn Gabor <katlyngabor@gmail.com>
2014-06-26 10:45:58 -0400

    fixed logo java

--- app/scripts/katlyn.js
+++ app/scripts/katlyn.js
@@ -1,3 +1,6 @@
 $('#logo').mouseenter(function(){
 	$(this).animate().html('Mystery Meat');
+	$(this).mouseleave(function(){
+	$(this).animate().html('¿MM?');	
+	});
 });
--- dist/scripts/main.js
+++ dist/scripts/main.js
@@ -1,13 +1,22 @@
-var menuTemplate=_.template( $('#menuTemplate').html() );
+// var menuTemplate=_.template( $('#menuTemplate').html() );
 
-var content= $.getJSON('http://restaurantapi.apiary.io/').done( function (data){
-	$('#menu').html( menuTemplate(data));
-});
-var flikrTemplate=_.template( $('#flikrTemplate').html() );
+// var content= $.getJSON('http://restaurantapi.apiary.io/').done( function (data){
+// 	$('#menu').html( menuTemplate(data));
+// });
 
-var images= $.getJSON('http://restaurantapi.apiary.io/').done( function (data){
-	$('#flikrImages').html(flikrTemplate(data));
-});
+var newsTemplate=_.template( $('#newsTemplate').html() );
+
+function stuff(a){
+	$('#news').html( newsTemplate(a) );
+
+}
+
+console.log(stuff(news));
+// var flikrTemplate=_.template( $('#flikrTemplate').html() );
+
+// var images= $.getJSON('http://restaurantapi.apiary.io/').done( function (data){
+// 	$('#flikrImages').html(flikrTemplate(data));
+// });
 function initialize () {
 	var map_canvas = document.getElementById('map_canvas');
 	var map_options = {
@@ -18,3 +27,15 @@ function initialize () {
 	var map = new google.maps.Map(map_canvas, map_options)
 }
 google.maps.event.addDomListener(window, 'load', initialize);
+$('#logo').mouseenter(function(){
+	$(this).animate().html('Mystery Meat');
+	$(this).mouseleave(function(){
+	$(this).animate().html('¿MM?');	
+	});
+});
+var news= {
+    "post_id": 13,
+    "title": "Acorn Finished Pork",
+    "post": "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum. Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
+
+}
