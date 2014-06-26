cae8aa591ea82b803d1e938630da5dd2ede90474
Taylor Smith <tsmit182@gmail.com>
2014-06-26 11:44:42 -0400

    used css: map now changes size and removes text on hover

--- app/scripts/mapAPI.js
+++ app/scripts/mapAPI.js
@@ -8,15 +8,3 @@ function initialize () {
 	var map = new google.maps.Map(map_canvas, map_options)
 }
 google.maps.event.addDomListener(window, 'load', initialize);
-
-// $('#map_canvas').hover()function({
-
-// 	$(this).addClass('hover');
-
-// })
-
-// ^^Check syntax, but this should work
-$('#map_canvas').hover(
-		function() {
-			$(this).addClass('hover');
-		}
