f6ff3ec0224bbf8067dc91fb835647a93ca7be9b
Katlyn Gabor <katlyngabor@gmail.com>
2014-06-27 10:48:29 -0400

    fixing merge conflicts

--- dist/scripts/main.js
+++ dist/scripts/main.js
@@ -1,17 +1,3 @@
-// var menuTemplate=_.template( $('#menuTemplate').html() );
-
-// var content= $.getJSON('http://restaurantapi.apiary.io/').done( function (data){
-// 	$('#menu').html( menuTemplate(data));
-// });
-
-var newsTemplate=_.template( $('#newsTemplate').html() );
-
-function stuff(a){
-	$('#news').html( newsTemplate(a) );
-
-}
-
-console.log(stuff(news));
 // var flikrTemplate=_.template( $('#flikrTemplate').html() );
 
 // var images= $.getJSON('http://restaurantapi.apiary.io/').done( function (data){
@@ -27,13 +13,3 @@ function initialize () {
 	var map = new google.maps.Map(map_canvas, map_options)
 }
 google.maps.event.addDomListener(window, 'load', initialize);
-
-$('#restaurant-name').mouseenter(function(){
-	$(this).animate().html('Always a treat.');
-	$(this).animate().css('font-size','50px');
-	$(this).mouseleave(function(){
-	$(this).animate({},'slow').html('Mystery Meat');
-	$(this).animate().css('font-size','70px')	
-
-	});
-});
