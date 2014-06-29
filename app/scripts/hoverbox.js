e329ae4063a4d6d67191e776a6b9923426a5fcc4
Katlyn Gabor <katlyngabor@gmail.com>
2014-06-29 18:51:27 -0400

    styled and added hover boxes

--- app/scripts/hoverbox.js
+++ app/scripts/hoverbox.js
@@ -1,12 +1,12 @@
 var allergy_info = "This item may contain shellfish or another item that some people may be allergic to. Please ask your waiter or waitress for assistance.";
 
 
-$('.fa-exclamation-triangle').hover(function () {
+$('.allergies-icon').hover(function () {
     
     $(this).append('<div>' + allergy_info + '</div>');
   
   }, function () {
   
-    $(this).find('span').remove();
+    //$(this).find('span').remove();
     
 });
--- app/scripts/restaurantapi.js
+++ app/scripts/restaurantapi.js
@@ -24,7 +24,64 @@ $.getJSON('http://restaurantapi.apiary.io/menu', function (menu){
  		
  	})
 
+}).done(function(){
+
+	var allergy_info = "This item may contain shellfish or another item that some people may be allergic to. Please ask your waiter or waitress for assistance.";
+
+
+$('.allergies-icon').hover(function () {
+    
+    $(this).append('<div class="allergy-info-box">' + '<span class="allergy-info-box-title">Allergies</span><br>' +  allergy_info + '</div>');
+  
+  }, function () {
+  
+    $(this).find('div').remove();
+    
 });
+
+var spicy_info = "This item is spicy, please handle with care and drink lots of water!";
+
+
+$('.spicy-icon').hover(function () {
+    
+    $(this).append('<div class="spicy-info-box">' + '<span class="spicy-info-box-title">Spicy!</span><br>' + spicy_info + '</div>');
+  
+  }, function () {
+  
+    $(this).find('div').remove();
+
+ });
+
+    var favorite_info = "We have been doing this a long time and this item has become one of our favorites.";
+
+
+$('.favorite-icon').hover(function () {
+    
+    $(this).append('<div class="favorite-info-box">' + '<span class="favorite-info-box-title">A Favorite!</span><br>' + favorite_info + '</div>');
+  
+  }, function () {
+  
+    $(this).find('div').remove();
+
+
+});
+
+
+    var vegan_info = "This item contains no meat and has been prepared without the use of animal products.";
+
+
+$('.vegan-icon').hover(function () {
+    
+    $(this).append('<div class="vegan-info-box">' + '<span class="vegan-info-box-title">Vegan Option</span><br>' + vegan_info + '</div>');
+  
+  }, function () {
+  
+    $(this).find('div').remove();
+
+
+	});
+});
+
 
 
 
